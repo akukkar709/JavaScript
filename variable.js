4720e2c13b5e2ea0465fc64157816b13112a9491
Aarish Kumar <akukkar709@gmail.com>
2026-01-02 15:59:54 +0530

    updated

---
+++ variable.js
@@ -0,0 +1,32 @@
+const accountId = 14553
+let accountEmail = "ak@google.com"
+var accountPassword = "12345"
+let accountCity = "jaipur"
+let accountState = "Rajsthan"
+
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
+// accountId = 45332  // not allowed Because one value already fixed and cons
+
+
+accountEmail = "hk@google.com"
+accountPassword = "67890"
+accountCity = "Fazilka Punjab"
+accountState = "Punjab"
+
+console.log(accountId)
+
+
+// use of table because of to print directly not by one    not doing one by one 
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState
+])
+
+
+// My personally experience to build anything when you create something in the developing only  use const and let    why not var because it changes in the functional scope and block scope
+
+
+ 
+
+
+
