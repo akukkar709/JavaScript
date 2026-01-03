2625258771f02c9ea366f667a313bec3026f3796
Aarish Kumar <akukkar709@gmail.com>
2026-01-03 09:47:25 +0530

    updated

---
+++ datatype.js
@@ -0,0 +1,28 @@
+"use Strict"  // treat all code of js in new version 
+
+// alert(3+3)   // we are using node js not browser 
+
+// my personal experience in programming  if you write code so it should be readable , future proof.....
+
+
+// data types
+
+// numbers 
+// int
+//strings
+//boolean
+//null
+//Symbol
+//undefined
+
+//object
+
+let name = "Aarish"
+
+console.log(typeof name)
+
+console.log(typeof null)   // type of null => object
+
+console.log(typeof undefined)  // type of undefined => undefined
+
+
