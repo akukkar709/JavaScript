689d85670d0869f7c10ab87aadbdf3435dc760af
Aarish Kumar <akukkar709@gmail.com>
2026-01-03 16:29:03 +0530

    update

---
+++ strings.js
@@ -0,0 +1,17 @@
+const name = "Aarish"
+
+const repocount = 50
+
+// console.log(name +" "+repocount+ " " + "value")  // outdated syntax
+
+console.log(`my name is ${name} and my repocount is ${repocount}`)
+
+
+const gameName = new String('abhimanyu')
+
+console.log(gameName)
+
+console.log(gameName[0])
+console.log(gameName[1])
+console.log(gameName[2])
+console.log(gameName[3])
