58bf8895f146445949e3e0ca31b3baf9740c858a
Aarish Kumar <akukkar709@gmail.com>
2026-01-03 21:37:33 +0530

    update

--- strings.js
+++ strings.js
@@ -15,3 +15,32 @@ console.log(gameName[0])
 console.log(gameName[1])
 console.log(gameName[2])
 console.log(gameName[3])
+
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length)
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName.toLowerCase())
+
+console.log(gameName.charAt(2))
+
+const newString = gameName.substring(0,4)
+
+console.log(newString)
+
+let anotherString = gameName.slice(-9,5)
+console.log(anotherString)
+
+
+let newOneString = "     Aarish      "
+
+console.log(newOneString)
+console.log(newOneString.trim())
+
+
+const url = "https://nk.com//nk%20nitesh"
+
+console.log(url.replace('%20','-'))
