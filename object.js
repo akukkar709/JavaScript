f7115dfe03a35597237a98f1a29722d39e646031
Aarish Kumar <akukkar709@gmail.com>
2026-01-05 18:09:06 +0530

    update

--- object.js
+++ object.js
@@ -34,5 +34,17 @@ console.log(regularUser.fullName.Username.LastName)
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"c",4:"d"}
 
-const obj3 = {obj1,obj2}
-console.log(obj3)
+// const obj3 = {obj1,obj2}
+
+const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3);
+
+const Users = [
+
+    {
+        id: 1,
+        email:"some@gmail.com",
+        isLoggedIn: false
+        
+    }
+]
