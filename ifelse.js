9a82893570ea33bee05d6c74c2766394f197f2b0
Aarish Kumar <akukkar709@gmail.com>
2026-01-06 22:34:44 +0530

    update

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+const isUserLoggedIn = true
+
+// if(3 != 2){
+
+// } 
+
+const temperature = 50
+if ( temperature < 50){
+    console.log("temperature is less than 50");
+}
+else{
+console.log("temperature is greater than 50");
+}
+
+
+const score = 200
+
+if(score>100){
+    
+}
