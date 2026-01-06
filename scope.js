80e3dd75eef324841df2a98ccde516effd642795
Aarish Kumar <akukkar709@gmail.com>
2026-01-06 19:42:03 +0530

    update

---
+++ scope.js
@@ -0,0 +1,18 @@
+// let a = 10;
+// var b = 20;
+// const c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+if (true){
+    let a = 10;
+    var b = 20;
+    const c = 30;
+}
+
+// console.log(a); // error 
+console.log(b); // 20
+// console.log(c); //error 
